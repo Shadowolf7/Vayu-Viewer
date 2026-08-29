e3a4f55ec9dc76ad4d67b1d43926cf2a3c2626c8
Roger Luedecke <roger.luedecke@gmail.com>
2026-08-28 17:09:42 -0700

    RLVa: Use LLStartUp and LLApp states to guard minimap floater during shutdown

--- indra/newview/rlvui.cpp
+++ indra/newview/rlvui.cpp
@@ -33,7 +33,7 @@
 #include "llvoavatar.h"
 #include "roles_constants.h"            // Group "powers"
 
-#include "llappviewer.h"
+#include "llstartup.h"
 #include "rlvui.h"
 #include "rlvhandler.h"
 #include "rlvextensions.h"
@@ -208,7 +208,7 @@ void RlvUIEnabler::onToggleShowMinimap()
     static bool fPrevVisibile = false;
     if ( (!fEnable) && ((fPrevVisibile = LLFloaterReg::instanceVisible("mini_map"))) )
         LLFloaterReg::hideInstance("mini_map");
-    else if ( (fEnable) && (fPrevVisibile) && (!LLApp::isExiting()) && (!LLAppViewer::instance() || !LLAppViewer::instance()->isDisconnected()) )
+    else if ( (fEnable) && (fPrevVisibile) && (LLStartUp::getStartupState() == STATE_STARTED) && (!LLApp::isExiting()) && (!LLApp::isQuitting()) )
         LLFloaterReg::showInstance("mini_map");
 
     // Break/reestablish the visibility connection for the nearby people panel embedded minimap instance
