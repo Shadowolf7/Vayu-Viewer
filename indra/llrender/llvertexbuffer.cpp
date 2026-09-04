f9a427c8b6e41e44ec0ea091f27bc2b7c448291d
gwigz <gwigz@users.noreply.github.com>
2026-09-04 16:26:26 +0100

    Fix Apple vertex buffer update crash
    Reuse existing vertex and index buffers on Apple GPUs and upload changes with glBufferSubData. Reserve glBufferData for the initial allocation to avoid driver crashes during geometry rebuilds.

--- indra/llrender/llvertexbuffer.cpp
+++ indra/llrender/llvertexbuffer.cpp
@@ -1463,14 +1463,22 @@ void LLVertexBuffer::_unmapBuffer()
         STOP_GLERROR;
         if (mMappedData)
         {
-            if (mGLBuffer)
+            if (!mGLBuffer)
             {
-                delete_buffers(1, &mGLBuffer);
+                mGLBuffer = gen_buffer();
+                glBindBuffer(GL_ARRAY_BUFFER, mGLBuffer);
+                sGLRenderBuffer = mGLBuffer;
+                glBufferData(GL_ARRAY_BUFFER, mSize, mMappedData, GL_STATIC_DRAW);
             }
-            mGLBuffer = gen_buffer();
-            glBindBuffer(GL_ARRAY_BUFFER, mGLBuffer);
-            sGLRenderBuffer = mGLBuffer;
-            glBufferData(GL_ARRAY_BUFFER, mSize, mMappedData, GL_STATIC_DRAW);
+            else
+            {
+                if (mGLBuffer != sGLRenderBuffer)
+                {
+                    glBindBuffer(GL_ARRAY_BUFFER, mGLBuffer);
+                    sGLRenderBuffer = mGLBuffer;
+                }
+                glBufferSubData(GL_ARRAY_BUFFER, 0, mSize, mMappedData);
+            }
         }
         else if (mGLBuffer != sGLRenderBuffer)
         {
@@ -1481,16 +1489,22 @@ void LLVertexBuffer::_unmapBuffer()
 
         if (mMappedIndexData)
         {
-            if (mGLIndices)
+            if (!mGLIndices)
             {
-                delete_buffers(1, &mGLIndices);
+                mGLIndices = gen_buffer();
+                glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mGLIndices);
+                sGLRenderIndices = mGLIndices;
+                glBufferData(GL_ELEMENT_ARRAY_BUFFER, mIndicesSize, mMappedIndexData, GL_STATIC_DRAW);
             }
-
-            mGLIndices = gen_buffer();
-            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mGLIndices);
-            sGLRenderIndices = mGLIndices;
-
-            glBufferData(GL_ELEMENT_ARRAY_BUFFER, mIndicesSize, mMappedIndexData, GL_STATIC_DRAW);
+            else
+            {
+                if (mGLIndices != sGLRenderIndices)
+                {
+                    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mGLIndices);
+                    sGLRenderIndices = mGLIndices;
+                }
+                glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, 0, mIndicesSize, mMappedIndexData);
+            }
         }
         else if (mGLIndices != sGLRenderIndices)
         {
@@ -1947,7 +1961,6 @@ void LLVertexBuffer::setIndexData(const U32* data, U32 offset, U32 count)
     }
     flush_vbo(GL_ELEMENT_ARRAY_BUFFER, offset * sizeof(U32), (offset + count) * sizeof(U32) - 1, (U8*)data, mMappedIndexData);
 }
-
 
 
 
